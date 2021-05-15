3d0e67fe3be407e762e27986b729c4d0a37ab89d
ryota35 <ryota35.1025@gmail.com>
2021-05-15 17:37:33 +0900

    標準入力を体験

--- self_study.rb
+++ self_study.rb
@@ -10,7 +10,13 @@ c = {name: "佐藤", age: 25, sex: "男"}
 puts c.values
 
 d = readlines
-d.each do |i|
-  puts i.to_s.chomp + "です"
-  puts "#{i.to_s.chomp}です"
+# d.each do |i|
+#   puts i.to_s.chomp + "です"
+#   puts "#{i.to_s.chomp}です"
+# end
+# p d[0].chomp
+i = 0
+while i <= 2 do
+  puts d[i].chomp
+  i += 1
 end
