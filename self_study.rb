c3b25dd425b30c56f9bf6a212b03713d5c5900eb
ryota35 <ryota35.1025@gmail.com>
2021-05-16 18:58:15 +0900

    test

--- self_study.rb
+++ self_study.rb
@@ -6,17 +6,25 @@ b.each do |x|
   puts 'おはよう' + x.to_s
 end
 
+
 c = {name: "佐藤", age: 25, sex: "男"}
 puts c.values
 
-d = readlines
-# d.each do |i|
-#   puts i.to_s.chomp + "です"
-#   puts "#{i.to_s.chomp}です"
+# d = readlines
+
+# # d.each do |i|
+# #   puts i.to_s.chomp + "です"
+# #   puts "#{i.to_s.chomp}です"
+# # end
+# # p d[0].chomp
+
+# i = 0
+# while i <= 2 do
+#   puts d[i].chomp
+#   i += 1
 # end
-# p d[0].chomp
-i = 0
-while i <= 2 do
-  puts d[i].chomp
-  i += 1
-end
+
+e = "foo"
+f = "hoo"
+
+puts e + f
