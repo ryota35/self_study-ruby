a1a4026a07b6f16ce3a42c649381e251677e670e
ryota35 <ryota35.1025@gmail.com>
2021-05-18 23:08:40 +0900

    メソッドサンプル

--- self_study.rb
+++ self_study.rb
@@ -29,15 +29,28 @@ f = "hoo"
 
 puts e + f
 
-g = gets.to_i
+# g = gets.to_i
 
-if g % 3 == 0 && g % 5 == 0 
-  puts 'FizzBuzz'
-elsif g % 3 == 0
-  puts 'Fizz'
-elsif g % 5 == 0
-  puts 'Buzz'
-else
-  puts '半角数字を入力してください'
-  g = gets.to_i
-end
+# if g % 3 == 0 && g % 5 == 0 
+#   puts 'FizzBuzz'
+# elsif g % 3 == 0
+#   puts 'Fizz'
+# elsif g % 5 == 0
+#   puts 'Buzz'
+# else
+#   puts '3でも5でも割り切れません'
+# end
+
+puts 1.22323454.floor
+puts -2.398374574985.floor
+
+puts 1.22323454.ceil
+puts -2.398374574985.ceil
+
+puts 1.62623454.round(2)
+puts -2.398374574985.round()
+
+puts 90 / 100.to_f
+
+g = "html, css, javascript, ruby, rails"
+p g.split(",")
