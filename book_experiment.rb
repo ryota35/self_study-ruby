b8edaeae375d73e3a1b968c6d148db7524e0d0d5
ryota35 <ryota35.1025@gmail.com>
2021-06-09 18:15:44 +0900

    途中経過

--- book_experiment.rb
+++ book_experiment.rb
@@ -455,3 +455,27 @@
 regexp = Regexp.new('Hello.Bye', Regexp::MULTILINE)
 p "Hello\nBye" =~ regexp
 
+class User
+  attr_reader :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+end
+
+users = []
+users << User.new('Alice', 'Ruby', 20)
+users << User.new('Bob', 'Python', 30)
+
+users.each do |user|
+  puts "氏名: #{user.full_name}、 年齢 #{user.age}"
+end
+
+
