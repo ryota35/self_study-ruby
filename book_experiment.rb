4d6cc4729ca7f2102037d2db72510e4850db693d
ryota35 <ryota35.1025@gmail.com>
2021-06-21 16:07:43 +0900

    途中経過

--- book_experiment.rb
+++ book_experiment.rb
@@ -868,3 +868,13 @@ p Date.today
 
 api1 = ENV['ryota']
 puts "#{api1}"
+
+p Date.new(2017, 1, 31)
+
+p DateTime.new(2017, 1, 31, 23, 30, 59)
+p Date.today
+
+api1 = ENV['ryota']
+puts "#{api1}"
+
+
